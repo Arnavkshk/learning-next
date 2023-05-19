082106182e845e0dde80dedc106cd60bf398a178
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-05-19 22:06:10 +0530

    added css and made blog component

--- huntingcoder/pages/blog.js
+++ huntingcoder/pages/blog.js
@@ -1,26 +1,35 @@
 import React from 'react'
+import styles from '@/styles/blog.module.css'
+import Link from 'next/link';
 
 const Blog = () => {
   return (
     <div>
       <div className="blogs">
-          <h2>Latest blogs</h2>
-         
-          <div className="blogitem">
-           <h3>how to learn javascript?</h3>
+          <main className={`${styles.main} `}>
+  
+          <div className={`${styles.blogitem}`}>
+          <Link href={'/blogpost/learn-javascript'}>
+            <h3 className={`${styles.blogitemh3}`}>how to learn javascript?</h3>
+          </Link>
            <p>javascript is the language used to design logic for the web </p> 
           </div>
-  
-          <div className="blogitem">
-           <h3>how to learn javascript?</h3>
+
+          <div className={`${styles.blogitem}`}>
+          <Link href={'/blogpost/learn-javascript'}>
+            <h3 className={`${styles.blogitemh3}`}>how to learn javascript?</h3>
+          </Link>
            <p>javascript is the language used to design logic for the web </p> 
           </div>
           
-          <div className="blogitem">
-           <h3>how to learn javascript?</h3>
+          <div className={`${styles.blogitem}`}>
+          <Link href={'/blogpost/learn-javascript'}>
+            <h3 className={`${styles.blogitemh3}`}>how to learn javascript?</h3>
+          </Link>
            <p>javascript is the language used to design logic for the web </p> 
           </div>
-        
+          
+        </main>
         </div>
     </div>
   )
