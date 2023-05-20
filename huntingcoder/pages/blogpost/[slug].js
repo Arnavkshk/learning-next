21b2c504e580abf7111fa5cfe73f0228fcef9f83
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-05-20 11:29:45 +0530

    added css and made slug component

--- huntingcoder/pages/blogpost/[slug].js
+++ huntingcoder/pages/blogpost/[slug].js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import styles from '@/styles/blogpost.module.css'
+
 
 const slug = () => {
     const router = useRouter();
     const {slug} = router.query;
   return (
-    <div>
-      {slug}
+    <main className={`${styles.main} `}>
+    <h1>Title of this page {slug}</h1>
+        <div className={`${styles.container}`}>
+
+     <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae, quia. Natus quae placeat pariatur dicta sapiente aut amet quisquam aperiam perferendis doloribus. Temporibus dolor libero nam delectus distinctio nulla quia cupiditate autem incidunt, doloremque vitae voluptatem, totam est eaque? Nulla voluptatum nisi cupiditate ut tenetur?</div>
     </div>
+      </main>
   )
 }
 
